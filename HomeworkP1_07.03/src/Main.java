887b86917d4a6940caf00b4dd115414e1a57539a
VitaliiMarchenko <125497857+VitaliiMarchenko@users.noreply.github.com>
2023-03-07 08:38:40 +0100

    Add files via upload

---
+++ HomeworkP1_07.03/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+    int number = 13;
+    double numerDouble = 3.5;
+    String string = "Hello Tel-Ran";
+    boolean booleanTF = false;
+
+    System.out.println(number);
+    System.out.println(numerDouble);
+    System.out.println(string);
+    System.out.println(booleanTF);
+    }
+}
